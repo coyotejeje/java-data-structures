e3ccdd7b6c2db9744eb7ab5446821d2862da932e
jerome <micl.jerome@gmail.com>
2022-03-30 13:05:23 +0200

    completed LinkedList chapter

--- src/main/java/fr/coyotejeje/WorkingWithQueues.java
+++ src/main/java/fr/coyotejeje/WorkingWithQueues.java
@@ -1,11 +1,30 @@
 package fr.coyotejeje;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("John", 21));
+        linkedList.addFirst(new Person("Mélanie", 21));
+
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while (personListIterator.hasPrevious()) {
+            System.out.println(personListIterator.previous());
+        }
+
+    }
+
+    private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
         supermarket.add(new Person("Alex", 21));
         supermarket.add(new Person("Miriam", 18));
